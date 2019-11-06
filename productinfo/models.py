e0cc83590237165c7b9130e2a3166f199b1b5ca9
AVE <AvinSJ>
2019-11-06 11:21:05 +0530

    cookies productinfo

--- productinfo/models.py
+++ productinfo/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class product (models.Model):
-    pid= models.CharField(max_length=10)
+
+class Product (models.Model):
+    pid = models.CharField(max_length=10)
     pname= models.CharField(max_length=100)
-    des=models.CharField(max_length=500)
-    price=models.CharField(max_length=500)
+    desc = models.CharField(max_length=500)
     price= models.IntegerField()
-    features =models.CharField(max_length=500)
+    features = models.CharField(max_length=500)
     mdate= models.DateField()
--- productinfo/views.py
+++ productinfo/views.py
@@ -4,26 +4,45 @@ from django.shortcuts import render
 
 from django.http import  HttpResponse
 from django.template import loader
-from .models import product
+from .models import Product
+
+
 
 def productInfo(req):
+
+    vals =req.GET
+    pid= vals['id']
+    print (pid)
+
     template=loader.get_template("productinfo.html")
-    data= {"name": "VIVO",
-           "desc": " Smart Phone",
-           "price": 45555}
-    return HttpResponse(template.render(data,req))
+
+    product = Product.objects.get(id=pid)
+    data= {"product":product}
+    #data={}
+    response= HttpResponse(template.render(data,req))
+
+    cstr= ""
+    print (req.COOKIES)
+    if "INT_PROD" in req.COOKIES:
+        old_ids = req.COOKIES['INT_PROD']
+        cstr= old_ids+ "|" + str ( product.id)
+    else:
+        cstr=product.id
+
+    response.set_cookie("INT_PROD", cstr )
+
+    return response
 
 
 
 def productList(req):
     template=loader.get_template("productList.html")
-    plist= product.objects.all()
+
+    plist = Product.objects.all()
+
     data= {
 
-        "products":plist
-
-
-
+        "products": plist
 
     }
     return HttpResponse(template.render(data,req))
